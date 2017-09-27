d0ea16b3f1009882394c73a363b95a3c7eae0aea
Rathi <github email address>
2017-09-27 13:28:54 -0400

    added babelrc

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,10 +7,23 @@ export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather(city){
   const url = `${ROOT_URL}&q=${city},us`
-  const request = axios.get(url);
+  console.log(axios.defaults.headers);
+
+  const request = axios.get(url)
+  
+
 
   return {
     type: FETCH_WEATHER,
     payload: request
   };
 }
+
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', "http://localhost");
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+// };
+// App.configure(function() {
+//     App.use(allowCrossDomain);
+// });
