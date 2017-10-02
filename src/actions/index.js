08e9bfc896700aab11d2cb817876f438c71d17b4
Rathi <github email address>
2017-10-02 15:08:33 -0400

    added reducer_weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,27 +2,16 @@ import axios from 'axios';
 import config from './config'
 
 var API_key = config.MY_KEY;
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?APPID=${API_key}`
-export const FETCH_WEATHER = 'FETCH_WEATHER'
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_key}`
+ export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather(city){
   const url = `${ROOT_URL}&q=${city},us`
 
   const request = axios.get(url)
-  console.log(axios.defaults.headers.common);
-
 
   return {
     type: FETCH_WEATHER,
     payload: request
   };
 }
-
-// var allowCrossDomain = function(req, res, next) {
-//     res.header('Access-Control-Allow-Origin', "http://localhost");
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-// };
-// App.configure(function() {
-//     App.use(allowCrossDomain);
-// });
